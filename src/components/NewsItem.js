f5a1548ce9348edb4585bed903c4265a2bd72229
Voltrix101 <amritob0327.roy@gmail.com>
2024-03-15 02:17:47 +0530

    minor changes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://www.yourtango.com/sites/default/files/image_blog/horoscope-march-14-2024-all-zodiac-signs-moon-enters-gemini.png":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
